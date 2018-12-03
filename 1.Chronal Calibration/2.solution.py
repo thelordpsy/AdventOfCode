cd76aee739d6afa634a7f52571c4a3515ca2d351
Jeffrey Brill <jbrill@blizzard.com>
2018-12-03 11:07:35 -0800

    Day 1 solutions

---
+++ 1.Chronal Calibration/2.solution.py
@@ -0,0 +1,19 @@
+current = 0
+history = {}
+history[current] = True
+
+def recycle_list(lst):
+    index = 0
+    while True:
+        yield lst[index]
+        index += 1
+        if index >= len(lst):
+            index = 0
+
+with open('input_file.txt', 'r') as file:
+    for value in recycle_list([int(line) for line in file.readlines()]):
+        current = current + value
+        if current in history:
+            print(current)
+            exit(0)
+        history[current] = True
